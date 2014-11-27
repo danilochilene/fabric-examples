c14fb4dbb8e536bf2e0d6af822f45b9c8b11d373
Danilo Chilene <bicofino@gmail.com>
2014-11-27 16:54:48 -0200

    wrap basic task functions

--- fabfile.py
+++ fabfile.py
@@ -12,25 +12,30 @@ env.roledefs = {
     'dbs': ['db01','db02','db03','db04'],                                         # My database servers
 }
 
+@task
 def mem_usage():
     '''Check free memory'''
     run('free -m')
 
+@task
 def sudo_test():
     '''Testing with sudo'''
     sudo('touch /root/myfile.txt')
     sudo('ls -la /root/myfile.txt')
 
+@task
 def put_file():
     '''Copy a local file to a server'''
     put('/tmp/myfile.txt', '/tmp/myfile.txt')
     run('ls /tmp/myfile.txt')
 
+@task
 def get_file():
     '''Copy a local file to a server'''
     get('/tmp/myfile.txt', '/tmp/myfile.txt')
     run('ls /tmp/myfile.txt')
 
+@task
 def read_key_file(key_file):
     key_file = os.path.expanduser(key_file)
     if not key_file.endswith('pub'):
@@ -38,6 +43,7 @@ def read_key_file(key_file):
     with open(key_file) as f:
         return f.read()
 
+@task
 def push_key(key_file='~/.ssh/id_rsa.pub'):
     '''Push a SSH key'''
     run("rm -rf /home/bicofino/.ssh")
@@ -46,23 +52,28 @@ def push_key(key_file='~/.ssh/id_rsa.pub'):
     run('touch ~/.ssh/authorized_keys')
     append('~/.ssh/authorized_keys', key_text)
 
+@task
 def start(service):
     '''Start a service, usage start:service'''
     run("/etc/init.d/{0} start".format(service),warn_only=True)
 
+@task
 def status(service):
     '''Get the status from a service, usage status:service'''
     run("/etc/init.d/{0} status".format(service),warn_only=True)
 
+@task
 def stop(service):
     '''Stop a service, usage stop:service'''
     run("/etc/init.d/{0} stop".format(service),warn_only=True)
 
+@task
 def restart(service):
     '''Restart a service, usage restart:service'''
     stop(service)
     start(service)
 
+@task
 def prompt_test():
     '''Just a user prompt test'''
     local = prompt('Type dir to list:')
