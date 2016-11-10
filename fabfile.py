e45f1cd5005d623dcc7096de2caf9ecf0a462aff
Danilo Chilene <bicofino@gmail.com>
2016-11-10 12:30:50 -0200

    (fix/pep8): code readability

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,25 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 """
     fabfile.py Example
     author: Danilo F. Chilene <bicofino at gmail dot com>
 """
-from fabric.api import run,sudo,put,prompt,cd,env,get,task
-from fabric.contrib.files import append,exists
-import os,sys
+from fabric.api import run, sudo, put, prompt, cd, env, get, task, lrun
+from fabric.contrib.files import append
+import os
 
 env.roledefs = {
-    'webservers': ['web01','web02','web03','web04'],                                         # My web servers
-    'dbs': ['db01','db02','db03','db04'],                                         # My database servers
+    # My web servers
+    'webservers': ['web01', 'web02', 'web03', 'web04'],
+    # My database servers
+    'dbs': ['db01', 'db02', 'db03', 'db04'],
 }
+
 
 @task
 def mem_usage():
     '''Check free memory'''
     run('free -m')
+
 
 @task
 def sudo_test():
@@ -23,17 +27,20 @@ def sudo_test():
     sudo('touch /root/myfile.txt')
     sudo('ls -la /root/myfile.txt')
 
+
 @task
 def put_file():
     '''Copy a local file to a server'''
     put('/tmp/myfile.txt', '/tmp/myfile.txt')
     run('ls /tmp/myfile.txt')
 
+
 @task
 def get_file():
     '''Copy a local file to a server'''
     get('/tmp/myfile.txt', '/tmp/myfile.txt')
     run('ls /tmp/myfile.txt')
+
 
 @task
 def read_key_file(key_file):
@@ -44,6 +51,7 @@ def read_key_file(key_file):
     with open(key_file) as f:
         return f.read()
 
+
 @task
 def push_key(key_file='~/.ssh/id_rsa.pub'):
     '''Push a SSH key'''
@@ -53,26 +61,31 @@ def push_key(key_file='~/.ssh/id_rsa.pub'):
     run('touch ~/.ssh/authorized_keys')
     append('~/.ssh/authorized_keys', key_text)
 
+
 @task
 def start(service):
     '''Start a service, usage start:service'''
-    run("/etc/init.d/{0} start".format(service),warn_only=True)
+    run("/etc/init.d/{0} start".format(service), warn_only=True)
+
 
 @task
 def status(service):
     '''Get the status from a service, usage status:service'''
-    run("/etc/init.d/{0} status".format(service),warn_only=True)
+    run("/etc/init.d/{0} status".format(service), warn_only=True)
+
 
 @task
 def stop(service):
     '''Stop a service, usage stop:service'''
-    run("/etc/init.d/{0} stop".format(service),warn_only=True)
+    run("/etc/init.d/{0} stop".format(service), warn_only=True)
+
 
 @task
 def restart(service):
     '''Restart a service, usage restart:service'''
     stop(service)
     start(service)
+
 
 @task
 def prompt_test():
@@ -82,10 +95,12 @@ def prompt_test():
     with cd(dir):
         run('ls')
 
+
 @task
 def install(package):
     '''Install a package / apt-get'''
     sudo("apt-get -y install %s" % package)
+
 
 @task
 def asm_disks():
@@ -98,34 +113,103 @@ def asm_disks():
     sudo('cat /etc/fstab')
     disks.append(sudo('/etc/init.d/oracleasm listdisks').splitlines())
     for disk in disks:
-         numbers = range(0, len(disk))
-         for number in numbers:
+        numbers = range(0, len(disk))
+        for number in numbers:
             devices = []
-            devices.append(sudo('/etc/init.d/oracleasm querydisk -d {0}'.format(disk[number])))
+            devices.append(
+                sudo('/etc/init.d/oracleasm querydisk -d {0}'.format(disk[number])))
             for device in devices:
                 partition = device.split('[', 1)[1].split(']')[0]
-                sudo('ls -l /dev |grep {0}|grep {1}'.format(partition.split(',')[0], partition.split(',')[1]))
+                sudo(
+                    'ls -l /dev |grep {0}|grep {1}'.format(
+                        partition.split(',')[0],
+                        partition.split(',')[1]))
+
 
 @task
-def remove_pkg_weblogic(domain,pkg,duser,dpassword,dhostname,cluster,classpath,dport):
+def remove_pkg_weblogic(
+        domain,
+        pkg,
+        duser,
+        dpassword,
+        dhostname,
+        cluster,
+        classpath,
+        dport):
     '''Remove a weblogic package(ear)'''
-    name = sudo('''cat {0}/config/config.xml |  grep {1}|grep -v argument|sed 's/\(.*<name>\)//' | sed 's/\(<\/name>*\)//'| head -1'''.format(domain,pkg))
-    sudo('''/u01/jdk/bin/java -classpath {5} weblogic.Deployer -adminurl t3://{0}:{6} -user {1} -password {2} -undeploy -name {3} -targets {4}'''.format(dhostname,duser,dpassword,name,cluster,classpath,dport))
+    name = sudo(
+        '''cat {0}/config/config.xml |  grep {1}|grep -v argument|sed 's/\(.*<name>\)//' | sed 's/\(<\/name>*\)//'| head -1'''.format(domain, pkg))
+    sudo(
+        '''/u01/jdk/bin/java -classpath {5} weblogic.Deployer -adminurl t3://{0}:{6} -user {1} -password {2} -undeploy -name {3} -targets {4}'''.format(
+            dhostname,
+            duser,
+            dpassword,
+            name,
+            cluster,
+            classpath,
+            dport))
+
 
 @task
-def deploy_weblogic(domain,pkg,classpath,duser,dpassword,upload,cluster,dhostname,dport,vm_admin):
+def deploy_weblogic(
+        domain,
+        pkg,
+        classpath,
+        duser,
+        dpassword,
+        upload,
+        cluster,
+        dhostname,
+        dport,
+        vm_admin):
     '''Deploy a package(ear) to weblogic'''
-    name = sudo('''ls {0}/servers/{2}/upload/{1}*'''.format(domain,pkg,vm_admin))
+    name = sudo(
+        '''ls {0}/servers/{2}/upload/{1}*'''.format(domain, pkg, vm_admin))
     name = os.path.basename(name)
-    sudo('''/u01/jdk/bin/java -classpath {0} weblogic.Deployer -adminurl t3://{1}:{7} -user {2} -password {3} -stage -distribute -name {4} "{5}{4}" -targets {6}'''.format(classpath,dhostname,duser,dpassword,name,upload,cluster,dport))
-    sudo('''/u01/jdk/bin/java -classpath {0} weblogic.Deployer -adminurl t3://{1}:{6} -user {2} -password {3} -start -name {4} -targets {5}'''.format(classpath,dhostname,duser,dpassword,name,cluster,dport))
+    sudo(
+        '''/u01/jdk/bin/java -classpath {0} weblogic.Deployer -adminurl t3://{1}:{7} -user {2} -password {3} -stage -distribute -name {4} "{5}{4}" -targets {6}'''.format(
+            classpath,
+            dhostname,
+            duser,
+            dpassword,
+            name,
+            upload,
+            cluster,
+            dport))
+    sudo(
+        '''/u01/jdk/bin/java -classpath {0} weblogic.Deployer -adminurl t3://{1}:{6} -user {2} -password {3} -start -name {4} -targets {5}'''.format(
+            classpath,
+            dhostname,
+            duser,
+            dpassword,
+            name,
+            cluster,
+            dport))
+
 
 @task
-def stop_weblogic(domain,pkg,duser,dpassword,dhostname,cluster,classpath,dport):
+def stop_weblogic(
+        domain,
+        pkg,
+        duser,
+        dpassword,
+        dhostname,
+        cluster,
+        classpath,
+        dport):
     '''Stop weblogic VM'''
-    sudo('''/u01/jdk/bin/java -classpath {4} weblogic.Admin -url t3://{0}:{5} -username {1} -password {2} STOPCLUSTER -clusterName {3}'''.format(dhostname,duser,dpassword,cluster,classpath,dport))
+    sudo(
+        '''/u01/jdk/bin/java -classpath {4} weblogic.Admin -url t3://{0}:{5} -username {1} -password {2} STOPCLUSTER -clusterName {3}'''.format(
+            dhostname,
+            duser,
+            dpassword,
+            cluster,
+            classpath,
+            dport))
+
 
 @task
-def start_weblogic(duser,dpassword,dhostname,dport,cluster):
+def start_weblogic(duser, dpassword, dhostname, dport, cluster):
     '''Start weblogic VM'''
-    lrun('''/home/oracle/start.sh -u {0} -p {1} -h {2}:{3} -c {4}'''.format(duser,dpassword,dhostname,dport,cluster))
+    lrun('''/home/oracle/start.sh -u {0} -p {1} -h {2}:{3} -c {4}'''.format(
+        duser, dpassword, dhostname, dport, cluster))
